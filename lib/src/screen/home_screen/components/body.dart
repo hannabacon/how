d4afcdae96f68252b10b67487aabc027e059652e
hannabacon <yasschetingergo@gmail.com>
2024-05-30 03:05:01 -0300

    feat: refactor HomeScreen screen

--- lib/src/screen/home_screen/components/body.dart
+++ lib/src/screen/home_screen/components/body.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:how/src/helper/enums.dart';
-import 'package:how/src/store/home_screen_store.dart';
+import 'package:how/src/store/home_store.dart';
 import 'package:how/src/widgets/receitas_loading.dart';
 
 class Body extends StatelessWidget {
@@ -11,38 +11,45 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Observer(builder: (context) {
-      if (store.statusPage == StatusPage.loading) {
-        return const ReceitasLoading();
-      }
+    return Scaffold(
+      body: Observer(builder: (context) {
+        if (store.statusPage == StatusPage.loading) {
+          return const ReceitasLoading();
+        }
 
-      if (store.statusPage == StatusPage.success) {
-        return ListView.builder(
-          itemCount: store.receitas.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text(store.receitas[index]['title']),
-              subtitle: Text(store.receitas[index]['subtitle']),
-              leading: Container(
-                width: 90,
-                height: 90,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(16),
-                  image: DecorationImage(
-                    image: AssetImage(store.receitas[index]['image']),
-                    fit: BoxFit.fitHeight,
+        if (store.statusPage == StatusPage.success) {
+          return ListView.builder(
+            itemCount: store.receitas.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(store.receitas[index]['title']),
+                subtitle: Text(store.receitas[index]['subtitle']),
+                leading: Container(
+                  width: 90,
+                  height: 90,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    image: DecorationImage(
+                      image: AssetImage(store.receitas[index]['image']),
+                      fit: BoxFit.fitHeight,
+                    ),
                   ),
                 ),
-              ),
-              trailing: IconButton(
-                onPressed: () {},
-                icon: const Icon(Icons.arrow_forward_ios_rounded),
-              ),
-            );
-          },
-        );
-      }
-      return const Center(child: Text('Algo deu errado'));
-    });
+                trailing: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.arrow_forward_ios_rounded),
+                ),
+              );
+            },
+          );
+        }
+        return const Center(child: Text('Algo deu errado'));
+      }),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.red,
+        onPressed: () => Navigator.pushNamed(context, '/create-recips'),
+        child: const Icon(Icons.add, color: Colors.white),
+      ),
+    );
   }
 }
